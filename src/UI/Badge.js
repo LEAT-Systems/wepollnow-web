1255043e77bf3187066e9b0ec78d4571b792f35b
kannyekong <31696884+kannyekong@users.noreply.github.com>
2022-10-08 20:05:51 +0100

    changed

--- src/UI/Badge.js
+++ src/UI/Badge.js
@@ -1,8 +1,10 @@
 import React from "react";
+import CalendarMonth from "@mui/icons-material/CalendarMonth";
 
 const Badge = (props) => {
   return (
-    <span className="inline-flex items-center justify-center px-4 py-1 text-lg font-semibold leading-none text-white bg-gray-600 rounded-full">
+    <span className="inline-flex items-center justify-center space-x-1 px-4 py-1 text-lg font-semibold leading-none text-black bg-yellow-200 rounded-full">
+      {CalendarMonth}
       {props.children}
     </span>
   );
