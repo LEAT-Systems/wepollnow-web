19de5b7f307d6733da925092ffeb26aaf48ab915
kannyekong <31696884+kannyekong@users.noreply.github.com>
2022-10-10 00:38:24 +0100

    changed

--- src/Pages/landingPages/gettingStarted-3.js
+++ src/Pages/landingPages/gettingStarted-3.js
@@ -73,11 +73,8 @@ const GettingStartedThree = () => {
               </button>
             )}
             {hasSubmitted && (
-              <Link to="/getting-started-four" type="submit">
-                <button
-                  type="submit"
-                  className="w-full p-4 px-8 rounded-lg bg-green-500 text-white hover:bg-green-600 hover:-translate-y-1"
-                >
+              <Link to="/getting-started-four">
+                <button className="w-full p-4 px-8 rounded-lg bg-green-500 text-white hover:bg-green-600 hover:-translate-y-1">
                   Continue
                 </button>
               </Link>
