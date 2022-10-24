b186e5f2e8b2dcbbe4b64d80a82f69c258d006ea
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-10-24 14:51:12 +0100

    Modified Login.js file

--- src/Components/Layout/Admin/Pages/Login.js
+++ src/Components/Layout/Admin/Pages/Login.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Nav from "../../Landing/mainNav";
-//
+
 const Login = () => {
   return (
     <>
-      <Nav />
-      <div className="flex flex-row items-center justify-center min-h-screen px-4 py-4 mx-auto bg-center bg-no-repeat bg-hero-pattern md:px-0">
-        <div className="w-full px-4 py-4 text-lg text-gray-700 border border-gray-500 rounded-lg shadow-2xl md:w-1/3 shadow-gray-200">
+      <div className="flex flex-row items-center justify-center min-h-screen px-4 py-4 mx-auto md:px-0">
+        <div className="w-full px-4 py-4 text-lg text-gray-700 border border-gray-500 rounded-lg shadow-2xl md:w-98 shadow-gray-200">
           <div className="p-6 space-y-4">
             <h1 className="text-2xl text-center">Login</h1>
             <p className="text-sm text-center md:text-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -7,7 +10,10 @@ module.exports = {
         "hero-pattern": "url('images/ng_1.png')",
         "polls-pattern": "url('images/bg_polls.png')",
       }),
-    },
+      spacing: {
+        98: "31rem",
+      },
+    }
   },
 
   plugins: [],
