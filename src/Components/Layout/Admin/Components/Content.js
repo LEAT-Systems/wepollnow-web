30b1322c66bd7560d8528d12867c93ee644321a2
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-10-29 02:13:22 +0100

    Delete duplicate components

--- src/Components/Layout/Admin/Components/Content.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageContent = (props) => {
-  return <div className="w-full h-full">{props.children}</div>;
-};
-
-export default PageContent;
--- src/Components/Layout/Admin/Dashboard/Blog/Blog.js
+++ src/Components/Layout/Admin/Dashboard/Blog/Blog.js
@@ -1,8 +1,8 @@
 import React from "react";
-import SideBar from "../Components/Sidebar";
-import Header from "../Components/Header";
-import PageContent from "../Components/Content";
-import Wrapper from "../Components/BodyWrapper";
+import SideBar from "../../Components/Sidebar";
+import Header from "../../Components/Header";
+import PageContent from "../../Components/Content";
+import Wrapper from "../../Components/BodyWrapper";
 import BlogContent from "../PageContent/BlogContent";
 //
 const Blog = () => {
