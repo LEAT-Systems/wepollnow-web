1fb92e969cfac696f2dd48ba5639f975989fa513
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-03 10:27:33 +0100

    Add active class to filter and toggleview tabs

--- src/Components/Layout/Admin/Dashboard/Polls/PollsHeader.js
+++ src/Components/Layout/Admin/Dashboard/Polls/PollsHeader.js
@@ -1,16 +1,46 @@
 /** @format */
 
-import { FilterList, Tune } from "@mui/icons-material";
-import React from "react";
+import { FilterListRounded, GridViewRounded, TuneRounded, ViewColumnRounded } from "@mui/icons-material";
+import React, { useState } from "react";
 import SearchBar from "../Search/SearchBar";
 
-const PollsHeader = ({ setSearchResult, tableData }) => {
+const PollsHeader = ({
+  setSearchResult,
+  tableData,
+  handleGrid,
+  handleList,
+}) => {
+  const [active, setActive] = useState(0)
+  const [filterIsActive, setFilterIsActive] = useState(0);
+
+  const handleActive = (index) => {
+    setActive(index)
+  }
+
+  const handleFilterActive = (index) => {
+    setFilterIsActive(index);
+    console.log(index);
+  }
+
+  const filterActive =
+    "icons hover:bg-blue-100 bg-blue-100 border border-1 p-2 cursor-pointer rounded-xl text-sm md:text-xl h-full mr-2";
+  
+  const filterNotActive =
+    "icons hover:bg-blue-100 border border-1 p-2 cursor-pointer rounded-xl text-sm md:text-xl h-full mr-2";
+  
+  const viewActive =
+    "bg-blue-100 hover:bg-blue-100 border border-1 rounded-xl p-2 cursor-pointer text-sm md:text-xl  h-full mr-2";
+  
+  const viewNotActive =
+    "hover:bg-blue-100 border border-1 rounded-xl p-2 cursor-pointer text-sm md:text-xl  h-full mr-2";
 
   return (
     <div>
       <>
         <header className='flex flex-row justify-between mt-4 my-auto place-items-center px-6 items-center'>
-          <h3 className='font-bold text-lg md:text-2xl capitalize p-4'>Manage polls</h3>
+          <h3 className='font-bold text-lg md:text-2xl capitalize p-4'>
+            Manage polls
+          </h3>
           <div className='flex'>
             <SearchBar
               placeholder='Search'
@@ -19,27 +49,45 @@ const PollsHeader = ({ setSearchResult, tableData }) => {
             />
             <nav className='pl-3 pr-4 h-full m-auto hidden md:flex'>
               <span
-                className={`icons bg-blue-100 border border-1 p-3 cursor-pointer rounded-xl text-sm md:text-xl h-full mr-2`}
+                className={
+                  filterIsActive === 1 ? filterActive : filterNotActive
+                }
+                onClick={() => {
+                  handleFilterActive(1);
+                }}
               >
-                <Tune />
+                <TuneRounded sx={{ fontSize: "1.9rem" }} />
               </span>
               <span
-                className={`icons border border-1 rounded-xl p-3 cursor-pointer text-sm md:text-xl h-full`}
+                className={
+                  filterIsActive === 2 ? filterActive : filterNotActive
+                }
+                onClick={() => {
+                  handleFilterActive(2);
+                }}
               >
-                <FilterList />
+                <FilterListRounded sx={{ fontSize: "1.9rem" }} />
               </span>
             </nav>
 
             <nav className='pl-3 h-full m-auto hidden md:flex'>
               <span
-                className={`icons border border-1 rounded-xl p-3 cursor-pointer text-sm md:text-xl  h-full mr-2`}
+                className={active === 0 ? viewActive : viewNotActive}
+                onClick={() => {
+                  handleActive(0);
+                  handleList();
+                }}
               >
-                <Tune />
+                <ViewColumnRounded sx={{ fontSize: "1.9rem" }} />
               </span>
               <span
-                className={`icons border border-1 rounded-xl p-3 cursor-pointer text-sm md:text-xl  h-full`}
+                className={active === 1 ? viewActive : viewNotActive}
+                onClick={() => {
+                  handleActive(1);
+                  handleGrid();
+                }}
               >
-                <FilterList />
+                <GridViewRounded sx={{ fontSize: "1.7rem" }} />
               </span>
             </nav>
           </div>
--- src/Components/Layout/Admin/Dashboard/Polls/PollsPageContentOne.js
+++ src/Components/Layout/Admin/Dashboard/Polls/PollsPageContentOne.js
@@ -5,22 +5,40 @@ import Header from "../../Header";
 import Data from '../../Data.json'
 import PollsHeader from "./PollsHeader";
 import Grid from "./Grid";
+import { AddOutlined, Close, Delete } from "@mui/icons-material";
+import { Modal } from "@mui/material";
 // import { getTableData } from "../api";
 
 
 
 const PollsPageContentOne = () => {
-  // const [open, setOpen] = useState(false);
-
-  // const modalHandler = () => {
-  //   setOpen(true);
-  // };
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
+  const [open, setOpen] = useState(false);
   const [ tableData, setTableData ] = useState([])
-  const [ searchResult, setSearchResult ] = useState([])
+  const [searchResult, setSearchResult] = useState([])
+
+  /* View State */
+  const [isGrid, setIsGrid] = useState(false)
+
+  const handleGrid = () => {
+    setIsGrid(true)
+  }
+
+  const handleList = () => {
+    setIsGrid(false)
+  }
+  
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const handleClose = () => {
+    setOpen(!open);
+  };
+
+  const isActiveClass =
+    "flex items-center justify-center rounded-full py-3 px-5 h-full cursor-pointer text-sm bg-green-500 text-white capitalize transition-all duration-400 ease-in-out hover:bg-green-500 hover:text-white hover:rounded-full";
+
+    const isNotActiveClass =
+      "flex items-center justify-center rounded-md py-3 px-5 h-full cursor-pointer text-sm bg-green-500 text-white capitalize transition-all duration-400 ease-in-out hover:bg-green-500 hover:text-white hover:rounded-full";
 
   useEffect(() => {
     /* For the api */
@@ -40,10 +58,138 @@ const PollsPageContentOne = () => {
   return (
     <main className='flex flex-col justify-center w-[98%]'>
       <Header />
-      <PollsHeader setSearchResult={setSearchResult} tableData={tableData} />
-      <Grid />
-      {/* <FilteredTable searchResult={searchResult} /> */}
-      {/* <Tables data={Data} /> */}
+
+      <div className='px-4 md:px-6 lg:px-12'>
+        <PollsHeader
+          setSearchResult={setSearchResult}
+          tableData={tableData}
+          handleGrid={handleGrid}
+          handleList={handleList}
+        />
+
+        {/* Data Table */}
+        <div className='flex flex-col text-[#082a0f] border-2 rounded-lg px-6 my-8 md:mt-14 mb-6'>
+          <div className='flex flex-row justify-between pt-4 px-2'>
+            <h2 className='font-extrabold text-[#082a0f] text-lg'>Polls</h2>
+            <button
+              className={open ? isActiveClass : isNotActiveClass}
+              onClick={handleOpen}
+            >
+              create poll
+            </button>
+          </div>
+
+          {isGrid ? (
+            <Grid handleOpen={handleOpen} />
+          ) : (
+            <Tables data={Data} />
+          )}
+        </div>
+
+        {/* <FilteredTable searchResult={searchResult} /> */}
+        {/* <Tables data={Data} /> */}
+
+        {/* Overlay */}
+        <Modal
+          open={open}
+          onClose={handleClose}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div className='flex flex-col items-center justify-center px-6 py-4 mx-auto h-auto w-[95%] sm:w-5/6 md:w-3/5 bg-white rounded-lg'>
+            <header className='flex justify-between items-center w-full pb-3 border-b-2 border-solid border-gray-300 mb-3'>
+              <h2 className='font-extrabold text-lg md:text-xl text-[#082a0f] capitalize'>
+                edit survey
+              </h2>
+              <button
+                className='flex items-center justify-center border border-1 rounded-md py-[2px] px-[2px] cursor-pointer text-sm md:text-base bg-[#fcf0f0] text-red-500'
+                onClick={handleClose}
+              >
+                <Close />
+              </button>
+            </header>
+
+            <div className='flex justify-between items-center w-full py-3 px-3 my-2 border-2 border-solid border-gray-300 rounded-lg'>
+              <h2 className='font-[600] text-base text-gray-400 capitalize'>
+                Transportation
+              </h2>
+              <button
+                className='flex items-center justify-center border border-1 rounded-md py-[2px] px-[2px] cursor-pointer text-sm md:text-base text-red-400'
+                onClick={handleClose}
+              >
+                <Delete />
+              </button>
+            </div>
+
+            <div className='flex justify-between items-center w-full py-3 px-3 my-2 border-2 border-solid border-gray-300 rounded-lg'>
+              <h2 className='font-[600] text-base text-gray-400 capitalize'>
+                Education
+              </h2>
+              <button
+                className='flex items-center justify-center border border-1 rounded-md py-[2px] px-[2px] cursor-pointer text-sm md:text-base text-red-400'
+                onClick={handleClose}
+              >
+                <Delete />
+              </button>
+            </div>
+
+            <div className='flex justify-between items-center w-full py-3 px-3 my-2 border-2 border-solid border-gray-300 rounded-lg'>
+              <h2 className='font-[600] text-base text-gray-400 capitalize'>
+                Security
+              </h2>
+              <button
+                className='flex items-center justify-center border border-1 rounded-md py-[2px] px-[2px] cursor-pointer text-sm md:text-base text-red-400'
+                onClick={handleClose}
+              >
+                <Delete />
+              </button>
+            </div>
+
+            <div className='flex justify-between items-center w-full py-3 px-3 my-2 border-2 border-solid border-gray-300 rounded-lg'>
+              <h2 className='font-[600] text-base text-gray-400 capitalize'>
+                Finance & Economy
+              </h2>
+              <button
+                className='flex items-center justify-center border border-1 rounded-md py-[2px] px-[2px] cursor-pointer text-sm md:text-base text-red-400'
+                onClick={handleClose}
+              >
+                <Delete />
+              </button>
+            </div>
+
+            <form className='flex justify-between items-center w-full my-2 '>
+              <input
+                className='font-[600] text-base text-gray-400 capitalize h-full w-full border-y-2 border-l-2 border-r-0 border-solid border-gray-300 rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none py-4 px-3'
+                placeholder='Add New Survey'
+              />
+
+              <button
+                className='flex items-center justify-center border border-1 py-4 px-3 h-full cursor-pointer text-sm md:text-base bg-green-500 text-white rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg'
+                onClick={handleClose}
+              >
+                <AddOutlined sx={{ marginRight: ".3rem" }} />
+                Add
+              </button>
+            </form>
+
+            {/* Buttons */}
+            <div className='flex justify-end items-center w-full my-2'>
+              <button
+                className='flex items-center justify-center border-2 border-gray-300 py-3 px-5 h-full cursor-pointer text-sm rounded-md capitalize mr-4 transition-all duration-400 ease-in-out hover:bg-[#f3dddd] hover:text-red-600 hover:rounded-full'
+                onClick={handleClose}
+              >
+                cancel
+              </button>
+              <button className='flex items-center justify-center rounded-md py-3 px-5 h-full cursor-pointer text-sm bg-green-500 text-white capitalize transition-all duration-400 ease-in-out hover:bg-green-500 hover:text-white hover:rounded-full'>
+                confirm
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </main>
   );
 };
