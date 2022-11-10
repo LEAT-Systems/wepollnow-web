92d68fd240c9bc1cf6a7612486d92aff18bc306e
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-10 14:00:15 +0100

    Create barChart

--- src/Components/Layout/Admin/Dashboard/Charts/BarChart.jsx
+++ src/Components/Layout/Admin/Dashboard/Charts/BarChart.jsx
@@ -4,8 +4,11 @@ import { Bar } from "react-chartjs-2";
 import { Chart, registerables, defaults, Legend } from 'chart.js';
 
 Chart.register(...registerables)
+/* Target the legend positioning */
 Chart.defaults.plugins.legend.position = "right";
-Chart.defaults.layout.padding = 40;
+/* Give all element a defualt padding or change chartjs defualt padding*/
+// Chart.defaults.layout.padding = 10;
+/* Target bar borderWidth */
 Chart.defaults.elements.bar.borderWidth = 0;
 const data = {
   labels: [
@@ -20,6 +23,7 @@ const data = {
     "CNC",
     "DPC",
   ],
+  /* This type of endpoint is used to perform group bar chart */
   datasets: [
     {
       label: "Age 18-29",
@@ -64,6 +68,7 @@ const data = {
   ],
 };
 
+/* To manipulate the legend position, ideally you can't change the position */
 const legendMarginRight = {
   id: "legendMarginRight",
   beforeInit(chart, argument, options) {
@@ -81,44 +86,46 @@ const legendMarginRight = {
 
 const BarChart = () => {
   return (
-    <div className='flex flex-col gap-7 justify-center align-center h-full w-full'>
-      <h1 className='text-2xl'>This is a Chart</h1>
-      <div className='w-3/4 h-3/4'>
+    <div className='flex flex-col justify-center align-center h-full w-full'>
+      <div className='w-full h-full border rounded '>
         <Bar
-          height={5}
-          width={10}
+          height={75}
+          width={150}
           data={data}
           plugins={[legendMarginRight]}
           options={{
+            /* Remove responsiveness */
+            responsive: true,
             // Add padding
             layout: {
-              padding: 50
+              padding: {
+                top: 30,
+                bottom: 20,
+                right: 20,
+                left: 30
+              }
+              
             },
             // Change the legend label boxes
             plugins: {
-              
               legend: {
-                position: "right",
-                align: "start",
-                rtl: false,
-                ltr: true,
+                position: "top",
+                align: "center",
                 labels: {
-                  color: '#082a0f',
+                  color: "#082a0f",
                   font: {
                     size: 14,
                   },
                   boxWidth: 10,
                   boxHeight: 7,
                   usePointStyle: true,
-                  pointStyle: 'circle',
+                  pointStyle: "circle",
                   pointStyleWidth: 10,
                   textAlign: "left",
-
-                  
-                }
-              }
+                },
+              },
             },
-           
+
             scales: {
               y: {
                 beginAtZero: true,
@@ -128,10 +135,10 @@ const BarChart = () => {
                   // 3. Add a "K" to tell the number equals a thousand or above
                   // 4. Add an "M" to tell the number equals a million or above
                   // 5. Add a "B" to tell the number equals a billion or above
-                  callback: function(value) {
+                  callback: function (value) {
                     // Step one
                     // this.getLabelForValue(value)
-                    
+
                     const valueLegend = this.getLabelForValue(value);
                     // Step two
                     const filterValue = valueLegend.replace(",", "");
@@ -139,16 +146,25 @@ const BarChart = () => {
                     if (filterValue.length <= 3) {
                       return filterValue;
                     }
-                    if ( filterValue.length >= 3 && filterValue.length <= 6) {
-                      return valueLegend.substr(0, 3) + "K"
+                    if (filterValue.length >= 3 && filterValue.length <= 6) {
+                      return valueLegend.substr(0, 3) + "K";
                     }
                     if (filterValue.length >= 7 && filterValue.length <= 9) {
-                      return valueLegend.substr(0, 4) + "M"
+                      return valueLegend.substr(0, 4) + "M";
                     }
-                  }
-                }
-              }
-            }
+                  },
+                },
+              },
+              /* padding */
+              // r: {
+              //   ticks: {
+              //     backdropPadding: {
+              //       x: 22,
+              //       y: 10
+              //     }
+              //   }
+              // }
+            },
           }}
         />
       </div>
