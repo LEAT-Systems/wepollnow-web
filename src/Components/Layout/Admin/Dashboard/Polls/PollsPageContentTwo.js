7dab2254ecdb508fdc68d1fd49c54e40fea25642
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-14 06:32:17 +0100

    Finish pollResult Stylings, create custom dropdown, implemented grid and table view, added piechart and barchart routing in dropdown

--- src/Components/Layout/Admin/Dashboard/Polls/PollsPageContentTwo.js
+++ src/Components/Layout/Admin/Dashboard/Polls/PollsPageContentTwo.js
@@ -1,23 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { ArrowUpward, CalendarMonthOutlined, People, PieChart as Pie } from "@mui/icons-material";
+import { ArrowUpward, CalendarMonthOutlined, People, Save } from "@mui/icons-material";
 import Header from "../../Header";
-import PieChart from "../Charts/PieChart";
-import LabelPluginProvider from "../Charts/LabelPluginProvider";
 import BarChart from "../Charts/BarChart";
+import PieChart from "../Charts/PieChart";
+import DropDown from "../DropDown/DropDown";
+import Tables from "../Tables/Tables";
+import Grid from "./Grid";
+import Data from '../../Data.json'
+import { NavLink } from "react-router-dom";
 
-//
-//
-//
+
+
 const PollsPageContentTwo = () => {
+  const [open, setOpen] = useState(false);
 
-  const day = new Date().getDate;
-  const month = new Date().getMonth;
-  const year = new Date().getFullYear;
+  /* View State */
+  const [isGrid, setIsGrid] = useState(false);
+  const [isBar, setIsBar] = useState(false);
+  const [isPie, setIsPie] = useState(false);
 
-    const data = [
-      {
+  const handleGrid = () => {
+    setIsGrid(true);
+    setIsBar(false);
+    setIsPie(false);
+  };
+
+  const handleList = () => {
+    setIsGrid(false);
+    setIsBar(false);
+    setIsPie(false);
+  };
+
+  const handleBar = () => {
+    setIsGrid(false);
+    setIsBar(true);
+    setIsPie(false);
+  };
+
+  const handlePie = () => {
+    setIsGrid(false);
+    setIsPie(true);
+    setIsBar(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+
+  const data = [
+    {
         id: 3,
         status: ["Upcoming", "Scheduled", "Concluded"],
         users: "235,436",
@@ -32,15 +65,17 @@ const PollsPageContentTwo = () => {
       <main className='max-h-screen px-4 md:px-6 lg:px-12 text-[#082a0f]'>
         <div className='flex flex-row items-center justify-between pt-2'>
           <div className='flex flex-col mt-3 my-auto place-items-start items-start w-full'>
-            <h2 className='font-bold text-xl lg:text-2xl md:text-[1.4rem] capitalize pt-4 pb-2 pl-0 w-full'>
+            <h2 className='font-extrabold text-xl lg:text-2xl md:text-[1.4rem] capitalize pt-4 pb-2 pl-0 w-full'>
               Polls Result
             </h2>
-            <div className='flex flex-row items-center justify-start'>
-              <Link to='/dashboard/polls'>
-                <p className='font-bold '>Manage Polls</p>
-              </Link>
-              <NavigateNextIcon />
-              <p className='font-bold'>Poll Result</p>
+            <div className='flex flex-row items-center justify-start text-base'>
+              <NavLink to='/dashboard/polls' activeClassName={null}>
+                <p className='font-bold text-[#616b62]'>Manage Polls</p>
+              </NavLink>
+              <NavigateNextIcon
+                sx={{ fontSize: "0.8rem", margin: "auto .7rem" }}
+              />
+              <p className='font-bold text-[#082b0e]'>Poll Result</p>
             </div>
           </div>
         </div>
@@ -50,24 +85,27 @@ const PollsPageContentTwo = () => {
           {/* First Card */}
           {data.map((data) => (
             <>
-              <div className='flex flex-col relative border-2 border-gray-400 bg-white rounded-lg p-5 w-full h-[9rem]'>
+              <div className='flex flex-col flex-1 relative border-2 border-gray-400 bg-white rounded-lg px-5 py-2 w-full h-[9rem]'>
                 <div className='w-full whitespace'>
-                  <h2 className='text-base font-extrabold pb-1 whitespace-nowrap'>
+                  <h2 className='text-base font-bold whitespace-nowrap'>
                     Presidential Poll
                   </h2>
-                  <span className='font-bold text-gray-500 text-[.75rem] capitalize flex-1'>
+                  <span className='font-bold text-gray-500 text-[.75rem] capitalize'>
                     National
                   </span>
                 </div>
 
-                <div className='flex justify-between git add src/p-1 w-full'>
-                  <h3 className='text-[.7rem]'>
-                    <CalendarMonthOutlined sx={{ fontSize: '0.7rem', margin: "auto" }} />
+                <div className='flex justify-between my-auto w-full'>
+                  <h3 className='text-[.9rem]'>
+                    <CalendarMonthOutlined
+                      sx={{
+                        fontSize: "0.9rem",
+                        margin: "auto .2rem .2rem auto",
+                      }}
+                    />
                     22/22/2022
                   </h3>
-                  <h3 className='text-[.7rem]'>
-                    {data.status[2]}
-                  </h3>
+                  <h3 className='text-base'>{data.status[2]}</h3>
                 </div>
               </div>
 
@@ -98,11 +136,8 @@ const PollsPageContentTwo = () => {
 
               {/* Third Card */}
               <div className='border-2 border-gray-400 bg-white text-[#082a0f] rounded-lg px-4 py-3 w-full h-[9rem]'>
-                <LabelPluginProvider>
-                 <PieChart /> 
-                </LabelPluginProvider>
-                  
-                </div>
+                <PieChart />
+              </div>
             </>
           ))}
         </div>
@@ -111,12 +146,42 @@ const PollsPageContentTwo = () => {
 
         <div className='px-2 md:px-4'>
           <div className='flex flex-col p-4 border rounded-lg'>
-            <div className='flex flex-row items-center justify-between'>
-              <p>Presidential Poll Result</p>
+            <div className='flex flex-row items-center justify-between mb-10'>
+              <p className='font-[800] text-[#082b0e]'>
+                Presidential Poll Result
+              </p>
+
+              <div className='flex gap-2'>
+                <span className='border rounded-md my-auto flex w-auto p-2 font-[500] text-[#616b62]'>
+                  Total: {Data.length}
+                </span>
+                <DropDown
+                  handleBar={handleBar}
+                  handlePie={handlePie}
+                  handleGrid={handleGrid}
+                  handleList={handleList}
+                />
+
+                <span
+                  download
+                  className='border rounded-md my-auto flex w-auto hover:cursor-pointer p-2 font-[500] text-[#616b62]'
+                >
+                  CSC
+                  <Save sx={{ fontSize: "1.2rem", marginLeft: "1rem" }} />
+                </span>
+              </div>
             </div>
 
             <div className=''>
-              <BarChart />
+              {isGrid ? (
+                <Grid data={Data} handleOpen={handleOpen} />
+              ) : isBar ? (
+                <BarChart />
+              ) : isPie ? (
+                <PieChart />
+              ) : (
+                <Tables data={Data} />
+              )}
             </div>
           </div>
         </div>
