0e228bbed58485440bee8351a37ac906338112ea
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-24 02:23:20 +0100

    Modified CreatePollContainer.jsx

--- src/Components/Layout/Admin/Dashboard/Modals/CreatePollsContainer.js
+++ src/Components/Layout/Admin/Dashboard/Modals/CreatePollsContainer.js
@@ -9,38 +9,16 @@ import axios from "axios";
 
 const CreatePollsContainer = ({ open, handleClose, modalData }) => {
   const [page, setPage] = useState(1);
-  const [pollType, setPollType] = useState([]);
-  const [createPollInfo, setCreatePollinfo] = useState([]);
 
-  
-  const getPollType = async () => {
-    axios
-      .get("https://wepollnow.azurewebsites.net/poll/poll_category_party/")
-      .then((res) => {
-        console.log(res);
-        pollType(res);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const CreatePoll = async () => {
-    axios
-      .get("https://wepollnow.azurewebsites.net/poll/poll_category_party/")
-      .then((res) => {
-        console.log(res);
-        createPollInfo(res);
-      })
-      .catch((err) => console.log(err));
-  };
-
-
-  useEffect(() => {
-    getPollType();
-    CreatePoll();
-  }, []);
-  
-
-  console.log(page);
+  // const CreatePoll = async () => {
+  //   axios
+  //     .post("https://wepollnow.azurewebsites.net/poll/poll_category_party/")
+  //     .then((res) => {
+  //       console.log(res);
+  //       setCreatePollinfo(res);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   const nextPage = () => {
     setPage(page + 1);
@@ -49,6 +27,7 @@ const CreatePollsContainer = ({ open, handleClose, modalData }) => {
   const prevPage = () => {
     setPage(page - 1);
   };
+
   const Forms = () => {
     if (page === 1)
       return (
@@ -84,7 +63,7 @@ const CreatePollsContainer = ({ open, handleClose, modalData }) => {
         }}
       >
         <main className='flex flex-col items-center justify-center px-6 py-4 my-auto mx-auto h-auto w-[95%] sm:w-5/6 md:w-3/5 bg-white rounded-lg overflow-y-auto'>
-          <header className='flex justify-between items-center w-full border-b-2 border-solid border-gray-300 mb-3 py-2'>
+          <header className='flex justify-between items-center w-full border-b-2 border-solid border-gray-300 py-2'>
             <h2 className='font-extrabold text-lg md:text-xl text-[#082a0f] capitalize'>
               create poll
             </h2>
