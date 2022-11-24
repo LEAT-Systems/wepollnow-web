1b08c00e50e925991be80acbee63ab29a28f3e28
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-24 01:23:12 +0100

    Edit Add Poll Modal content

--- src/Components/Layout/Admin/Dashboard/Modals/AddPollModal.js
+++ src/Components/Layout/Admin/Dashboard/Modals/AddPollModal.js
@@ -1,5 +1,14 @@
+/** @format */
+
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import React, { useState } from "react";
 import APC from "../../../../../images/apc.png";
+import { ArrowBack } from "@mui/icons-material";
 
 const data = [
   {
@@ -26,8 +35,22 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData }) => {
     <>
       <form
         onSubmit={handleSubmit}
-        className='flex flex-col justify-between items-center w-full my-2 hover:bg-transparent'
+        className='flex justify-start flex-col items-center w-full hover:bg-transparent'
       >
+        <div className='w-full'>
+          <button
+            className='flex items-center justify-start rounded-md py-3 h-full cursor-pointer text-sm font-bold capitalize'
+            onClick={() => {
+              prevPage();
+            }}
+          >
+            <ArrowBack
+              sx={{ width: "1.5rem", height: "1.2rem", background: "#EDFFF0", padding: ".2rem", marginRight: '.5rem', borderRadius: "2rem" }}
+            />{" "}
+            Back
+          </button>
+        </div>
+
         {/* First Form */}
 
         {data?.map((data) => {
@@ -40,45 +63,68 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData }) => {
                 <img
                   src={data.party__image}
                   alt='Political Party'
-                  className='w-[3rem] aspect-square rounded-sm'
+                  className='w-[2.3rem] aspect-square rounded-sm'
                 />
-                <h2 className='text-lg px-4'>{data.party__name}</h2>
+                <h2 className='text-base px-4 text-[#616b62]'>
+                  {data.party__name}
+                </h2>
               </div>
-              <div className='flex items-center w-full border-b my-auto pb-2'>
-                <label className='custom__select__container'>
-                  <select
-                    name='candidate__name'
-                    id='candidate__name'
-                    value={candidateName}
-                    onChange={(e) => setCandidateName(e.target.value)}
-                    className='custom__select'
+
+              {/* Second Form */}
+              <div className='flex flex-col my-2 justify-start items-start w-full gap-3'>
+                <div className='w-full'>
+                  {/* Title */}
+                  <FormControl
+                    onChange={(e) => console.log(e.target.value)}
+                    sx={{ width: "100%" }}
                   >
-                    <option value='Select Candidate'>
-                      Select Poll Candidate
-                    </option>
-                    {data.candidate.map((candidate) => {
-                      return <option key={candidate.id}>{candidate}</option>;
-                    })}
-                  </select>
-                </label>
-              </div>
-              <div className='flex items-center w-full border-b my-auto pb-2'>
-                <label className='custom__select__container'>
-                  <select
-                    name='running__mate'
-                    id='running__mate'
-                    value={runningMate}
-                    onChange={(e) => setRunningMate(e.target.value)}
-                    className='custom__select'
-                  >
-                    <option value='Select Running Mate'>
-                      Select Poll Running Mate
-                    </option>
-                    {data.running__mate.map((candidate) => {
-                      return <option key={candidate.id}>{candidate}</option>;
-                    })}
-                  </select>
-                </label>
+                    <RadioGroup>
+                      <div className='flex justify-between align-center'>
+                        <FormControlLabel
+                          value='first__time'
+                          className='text-[#616b62] font-medium'
+                          sx={{ width: "100%" }}
+                          control={
+                            <Radio
+                              sx={{
+                                color: "#616b62",
+                                "&.Mui-checked": {
+                                  color: "#616b62",
+                                },
+                              }}
+                            />
+                          }
+                          label='Jane Doe'
+                        />
+                        <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
+                          Main
+                        </h3>
+                      </div>
+                      <div className='flex justify-between align-center'>
+                        <FormControlLabel
+                          value='returning__voter'
+                          className='text-[#616b62] font-medium'
+                          sx={{ width: "100%" }}
+                          control={
+                            <Radio
+                              sx={{
+                                color: "#616b62",
+                                "&.Mui-checked": {
+                                  color: "#616b62",
+                                },
+                              }}
+                            />
+                          }
+                          label='John Doe'
+                        />
+
+                        <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
+                          Running Mate
+                        </h3>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                </div>
               </div>
             </div>
           );
@@ -86,14 +132,6 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData }) => {
 
         {/* Buttons */}
         <div className='flex justify-end items-center w-full my-2'>
-          <button
-            className='flex items-center justify-center rounded-md py-3 px-5 h-full cursor-pointer text-sm text-green-500 capitalize transition-all duration-400 ease-in-out hover:bg-green-500 hover:text-white hover:rounded-full'
-            onClick={() => {
-              prevPage();
-            }}
-          >
-            Back
-          </button>
           <button
             className='flex items-center justify-center border-2 border-gray-300 py-3 px-5 h-full cursor-pointer text-sm rounded-md capitalize mr-4 transition-all duration-400 ease-in-out hover:bg-[#f3dddd] hover:text-red-600 hover:rounded-full'
             onClick={handleClose}
