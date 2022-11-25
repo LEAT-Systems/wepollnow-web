014ab1421bf7890472a6f407805c70a8ca734afc
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-11-25 12:10:31 +0100

    Added context in file

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { IdProvider } from "./Components/Forms/FormContext/userIdContext";
 import { AuthProvider } from "./AuthProvider";
+import { ModalFormProvider } from "./ModalFormContextAdmin/ModalFormContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <AuthProvider>
-    <BrowserRouter>
-      <IdProvider>
-        <App />
-      </IdProvider>
-    </BrowserRouter>    
+      <BrowserRouter>
+        <IdProvider>
+          <ModalFormProvider>
+            <App />
+          </ModalFormProvider>
+        </IdProvider>
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
