272d419801e9b27c7a05358d91c71dc00cfbcf6d
kannyekong <31696884+kannyekong@users.noreply.github.com>
2022-11-29 18:06:37 +0100

    fix: fixed bug in table body

--- src/Components/Layout/Admin/Dashboard/Tables/TableBody.js
+++ src/Components/Layout/Admin/Dashboard/Tables/TableBody.js
@@ -8,12 +8,11 @@ const TableBody = ({ tableData }) => {
   const getSymbol = () => {
     const string = tableData.poll_name;
     const wordArray = string.split(" ", 2);
-    const symbol = wordArray[0].slice(0, 1) + wordArray[1].slice(0, 1);
+    const symbol = wordArray[0].slice(0, 2);
+    // + wordArray[1].slice(0, 1);
     return symbol;
   };
 
-  
-  
   // console.log(tableData);
   // {
   //     "id": 1,
@@ -30,47 +29,47 @@ const TableBody = ({ tableData }) => {
   //     "status": 1
   // },
   return (
-    <tr className='table-row' onClick={(e) => console.log(e.currentTarget)}>
+    <tr className="table-row" onClick={(e) => console.log(e.currentTarget)}>
       <th
-        scope='row'
-        className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
       >
-        <span className='text-white font-bold capitalize bg-green-900 mr-4 rounded-xl p-[.6rem] cursor-pointer'>
+        <span className="text-white font-bold capitalize bg-green-900 mr-4 rounded-xl p-[.6rem] cursor-pointer">
           {getSymbol()}
         </span>{" "}
-        <NavLink to='/admin/polls/poll_result' activeClassName={null}>
+        <NavLink to="/admin/polls/poll_result" activeClassName={null}>
           {tableData.poll_name}
         </NavLink>
       </th>
-      <td className='px-6 py-4 cursor-pointer'>
-        <NavLink to='/admin/polls/poll_result' activeClassName={null}>
+      <td className="px-6 py-4 cursor-pointer">
+        <NavLink to="/admin/polls/poll_result" activeClassName={null}>
           {tableData.poll_state}
         </NavLink>
       </td>
-      <td className='px-6 py-4 cursor-pointer'>
-        <NavLink to='/admin/polls/poll_result' activeClassName={null}>
+      <td className="px-6 py-4 cursor-pointer">
+        <NavLink to="/admin/polls/poll_result" activeClassName={null}>
           {tableData.poll_startDate}
         </NavLink>
       </td>
-      <td className='px-6 py-4 cursor-pointer'>
-        <NavLink to='/admin/polls/poll_result' activeClassName={null}>
+      <td className="px-6 py-4 cursor-pointer">
+        <NavLink to="/admin/polls/poll_result" activeClassName={null}>
           {tableData.poll_endDate}
         </NavLink>
       </td>
-      <td className='px-6 py-4 cursor-pointer'>
-        <NavLink to='/admin/polls/poll_result' activeClassName={null}>
+      <td className="px-6 py-4 cursor-pointer">
+        <NavLink to="/admin/polls/poll_result" activeClassName={null}>
           {tableData.status === 1 ? "Active" : "Inactive"}
         </NavLink>
       </td>
-      <td className='flex flex-row px-6 py-4 space-x-2'>
-        <div className='text-blue-500 cursor-pointer'>
-          <img src={Edit} alt='Edit' className='w-[1.1rem] h-[1.1rem]' />
+      <td className="flex flex-row px-6 py-4 space-x-2">
+        <div className="text-blue-500 cursor-pointer">
+          <img src={Edit} alt="Edit" className="w-[1.1rem] h-[1.1rem]" />
         </div>
-        <div className='text-black cursor-pointer'>
-          <img src={Archive} alt='Archive' className='w-[1.1rem] h-[1.1rem]' />
+        <div className="text-black cursor-pointer">
+          <img src={Archive} alt="Archive" className="w-[1.1rem] h-[1.1rem]" />
         </div>
-        <div className='text-red-500 cursor-pointer'>
-          <img src={Delete} alt='Trash' className='w-[1.1rem] h-[1.1rem]' />
+        <div className="text-red-500 cursor-pointer">
+          <img src={Delete} alt="Trash" className="w-[1.1rem] h-[1.1rem]" />
         </div>
       </td>
     </tr>
