489fa25447c781401a10d86b9995457dc3a404b3
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-12-03 03:39:59 +0100

    Render candidates conditionally

--- src/Components/Layout/Admin/Dashboard/Modals/AddPollModal.js
+++ src/Components/Layout/Admin/Dashboard/Modals/AddPollModal.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useContext, useEffect, useState } from "react";
 import { ArrowBack } from "@mui/icons-material";
 import ModalFormContext from "../../../../../ModalFormContextAdmin/ModalFormContext";
@@ -15,7 +17,14 @@ import swal from "sweetalert";
 //   },
 // ];
 
-const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPage }) => {
+const AddPollModal = ({
+  open,
+  handleClose,
+  nextPage,
+  prevPage,
+  modalData,
+  setPage,
+}) => {
   const {
     pollType,
     selectedState,
@@ -39,17 +48,15 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPag
     let newPartyArray = [];
 
     parties?.forEach((element) => {
-
       for (var i = 0; i <= element.partyCandidate; i++) {
         let name = i.id;
         // Object.keys(name);
         newNameArray.push(name);
       }
-        
+
       // var name1 = element.partyCandidate[0].id;
       // var name2 = element.partyCandidate[1].id;
       var par = element.id;
-      
 
       // Object.keys(name1);
       // Object.keys(name2);
@@ -66,7 +73,6 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPag
       // } else {
       //   names = [name1, name2];
       // }
-      
     });
     setCandi(newNameArray);
     setParti(newPartyArray);
@@ -136,66 +142,63 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPag
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-
-      await axios
-        .post(`https://wepollnow.azurewebsites.net/poll/create_poll/`, config())
-        // .then((res) => {
-        //   setSuccessMessage(res.status);
-        //   console.log(res.data);
-        // })
-        // .catch((err) => console.log(err));
-        .then((res) => {
-          console.log(res);
+    await axios
+      .post(`https://wepollnow.azurewebsites.net/poll/create_poll/`, config())
+      // .then((res) => {
+      //   setSuccessMessage(res.status);
+      //   console.log(res.data);
+      // })
+      // .catch((err) => console.log(err));
+      .then((res) => {
+        console.log(res);
+        swal({
+          title: "Success",
+          text: "New Poll Added Successfully!",
+          icon: "success",
+          button: "Ok",
+        });
+        setPollType("");
+        setSelectedState("");
+        setDistrict("");
+        setStartDate("");
+        setEndDate("");
+        setSuccessMessage(true);
+      })
+      .catch((err) => {
+        if (!err?.response) {
+          setSuccessMessage("No Connection");
           swal({
             title: "Success",
-            text: "New Poll Added Successfully!",
-            icon: "success",
+            text: "No Internet Connection",
+            icon: "error",
             button: "Ok",
           });
-          setPollType("");
-          setSelectedState("");
-          setDistrict("");
-          setStartDate("");
-          setEndDate("");
-          setSuccessMessage(true);
-        })
-        .catch((err) => {
-          if (!err?.response) {
-            setSuccessMessage("No Connection");
-            swal({
-              title: "Success",
-              text: "No Internet Connection",
-              icon: "error",
-              button: "Ok",
-            });
-          } else if (err.response?.status === 400) {
-            setSuccessMessage("Email and Password are required");
-            swal({
-              title: "Failure",
-              text: "All fields are required!",
-              icon: "error",
-              button: "Ok",
-            });
-          } else if (err.response?.status === 401) {
-            setSuccessMessage("Unauthorized");
-            swal({
-              title: "Failure",
-              text: "Unauthorized",
-              icon: "error",
-              button: "Ok",
-            });
-          } else {
-            setSuccessMessage("Add Candidate Failed");
-            swal({
-              title: "Failure",
-              text: "Adding New Poll Failed",
-              icon: "error",
-              button: "Ok",
-            });
-          }
-        });
-
-
+        } else if (err.response?.status === 400) {
+          setSuccessMessage("Email and Password are required");
+          swal({
+            title: "Failure",
+            text: "All fields are required!",
+            icon: "error",
+            button: "Ok",
+          });
+        } else if (err.response?.status === 401) {
+          setSuccessMessage("Unauthorized");
+          swal({
+            title: "Failure",
+            text: "Unauthorized",
+            icon: "error",
+            button: "Ok",
+          });
+        } else {
+          setSuccessMessage("Add Candidate Failed");
+          swal({
+            title: "Failure",
+            text: "Adding New Poll Failed",
+            icon: "error",
+            button: "Ok",
+          });
+        }
+      });
   };
 
   return (
@@ -241,25 +244,31 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPag
               </div>
 
               {/* Second Form */}
+              {data.partyCandidate.length > 1 ? (
+                <div className='w-full'>
+                  <div className='flex justify-between align-center'>
+                    <h3 className='font-bold my-auto text-base text-[#000] whitespace-nowrap'>
+                      {data?.partyCandidate[0].name}
+                    </h3>
+                    <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
+                      Main
+                    </h3>
+                  </div>
 
-              <div className='w-full'>
-                <div className='flex justify-between align-center'>
-                  <h3 className='font-bold my-auto text-base text-[#000] whitespace-nowrap'>
-                    {data?.partyCandidate[0].name}
-                  </h3>
-                  <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
-                    Main
-                  </h3>
+                  <div className='flex justify-between align-center'>
+                    <h3 className='font-bold my-auto text-base text-[#000] whitespace-nowrap'>
+                      {data?.partyCandidate[1].name}
+                    </h3>
+                    <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
+                      Running Mate
+                    </h3>
+                  </div>
                 </div>
-                <div className='flex justify-between align-center'>
-                  <h3 className='font-bold my-auto text-base text-[#000] whitespace-nowrap'>
-                    {data?.partyCandidate[1].name}
-                  </h3>
-                  <h3 className='font-bold my-auto text-sm text-[#616b62] whitespace-nowrap'>
-                    Running Mate
-                  </h3>
-                </div>
-              </div>
+              ) : (
+                  <h3 className='w-full font-base text-red-400 text-center'>
+                    No candidate exist for intended poll!
+                </h3>
+              )}
             </div>
           );
         })}
@@ -269,7 +278,7 @@ const AddPollModal = ({ open, handleClose, nextPage, prevPage, modalData, setPag
           <button
             className='flex items-center justify-center border-2 border-gray-300 py-3 px-5 h-full cursor-pointer text-sm rounded-md capitalize mr-4 transition-all duration-400 ease-in-out hover:bg-[#f3dddd] hover:text-red-600 hover:rounded-full'
             onClick={() => {
-              handleClose()
+              handleClose();
               setPollType("");
               setSelectedState("");
               setDistrict("");
