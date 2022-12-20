669c1efc083d0a4f90e0ca2d481949c5bcc60deb
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-12-20 09:28:08 +0100

    Fix conditional rendering of poll result

--- src/Components/Layout/Admin/Dashboard/Tables/TableBody.js
+++ src/Components/Layout/Admin/Dashboard/Tables/TableBody.js
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+/** @format */
+
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Edit from "../../assets/edit@2x.png";
 // import Archive from "../../assets/archive@2x.png";
@@ -9,11 +11,22 @@ import axios from "axios";
 
 const TableBody = ({ tableData, open }) => {
   const { tableRowID, setTableRowID } = useContext(ModalFormContext);
-  const history = useHistory()
+  const [value, setValue] = useState("");
+
+  const getLocalstorageItem = () => {
+    const items = localStorage.getItem("pollID");
+    return items;
+  };
+
+  const saveToLocalstorage = () => {
+    localStorage.setItem("pollID", value);
+  };
+
+  const history = useHistory();
 
   const redirect = () => {
     history.push("/admin/polls/poll_result");
-  }
+  };
   const getSymbol = () => {
     const string = tableData.poll_name;
     const wordArray = string.split(" ", 2);
@@ -88,12 +101,18 @@ const TableBody = ({ tableData, open }) => {
 
     window.location.reload();
   };
+
   return (
-    <tr className='table-row cursor-pointer' data-id={tableData.id} onClick={(e) => {
-      setTableRowID(e.target.getAttribute('data-id'))
-      redirect()
-    }
-    }>
+    <tr
+      className='table-row cursor-pointer'
+      data-id={tableData.id}
+      onClick={(e) => {
+        setValue(e.target.getAttribute("data-id"));
+        saveToLocalstorage();
+        setTableRowID(getLocalstorageItem());
+        redirect();
+      }}
+    >
       <th
         scope='row'
         className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-sm'
@@ -101,33 +120,38 @@ const TableBody = ({ tableData, open }) => {
         <span className='text-white font-bold uppercase bg-green-900 mr-4 rounded-xl p-[.6rem]'>
           {getSymbol()}
         </span>{" "}
-          {tableData?.poll_name}
+        {tableData?.poll_name}
       </th>
       <td className='px-6 py-4 cursor-pointer text-sm'>
-          {tableData?.poll_state?.name}
+        {tableData?.poll_state?.name}
       </td>
       <td className='px-6 py-4 cursor-pointer text-sm'>
-          {formatDate(tableData?.poll_startDate)}
+        {formatDate(tableData?.poll_startDate)}
       </td>
       <td className='px-6 py-4 cursor-pointer text-sm'>
-          {formatDate(tableData?.poll_endDate)}
+        {formatDate(tableData?.poll_endDate)}
       </td>
       <td className='px-6 py-4 cursor-pointer'>
-          <h3
-            className={`relative after:content-[''] after:absolute after:w-[.6rem] after:h-[.6rem] after:rounded-full ${statusColors} after:-left-3 after:top-1/2 after:-translate-y-1/2`}
-          >
-            {tableData.status === 1
-              ? "Upcoming"
-              : tableData.status === 2
-              ? "Scheduled"
-              : "Concluded"}
-          </h3>
+        <h3
+          className={`relative after:content-[''] after:absolute after:w-[.6rem] after:h-[.6rem] after:rounded-full ${statusColors} after:-left-3 after:top-1/2 after:-translate-y-1/2`}
+        >
+          {tableData.status === 1
+            ? "Upcoming"
+            : tableData.status === 2
+            ? "Scheduled"
+            : "Concluded"}
+        </h3>
       </td>
-      <td className='flex flex-row px-6 py-4 space-x-2' onClick={(e) => e.stopPropagation()}>
+      <td
+        className='flex flex-row px-6 py-4 space-x-2'
+        onClick={(e) => e.stopPropagation()}
+      >
         <div
           className='text-blue-500 cursor-pointer'
           onClick={(e) => {
-            setTableRowID(parentTarget(e));
+            setValue(setTableRowID(parentTarget(e)));
+            saveToLocalstorage();
+            setTableRowID(getLocalstorageItem());
             // console.log(parentTarget(e));
             open();
           }}
@@ -140,7 +164,9 @@ const TableBody = ({ tableData, open }) => {
         <div
           className='text-red-500 cursor-pointer'
           onClick={(e) => {
-            setTableRowID(parentTarget(e));
+            setValue(setTableRowID(parentTarget(e)));
+            saveToLocalstorage();
+            setTableRowID(getLocalstorageItem());
             handleDelete();
             // console.log(parentTarget(e));
           }}
