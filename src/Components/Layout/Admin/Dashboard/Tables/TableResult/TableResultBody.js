36020cee558ee44828e2077669a0db1647555990
OKEREKE VINCENT <Vincentcode0@gmail.com>
2023-01-11 08:28:08 +0100

    Fix: symbol issue

--- src/Components/Layout/Admin/Dashboard/Tables/TableResult/TableResultBody.js
+++ src/Components/Layout/Admin/Dashboard/Tables/TableResult/TableResultBody.js
@@ -30,7 +30,7 @@ const TableResultBody = ({ tableData }) => {
         scope='row'
         className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '
       >
-        <span className='text-white font-bold capitalize bg-green-900 mr-4 rounded-xl p-[.6rem]'>
+        <span className='text-white font-bold uppercase bg-green-900 mr-4 rounded-xl p-[.6rem]'>
           {getSymbol()}
         </span>{" "}
         {`${tableData?.name} (${tableData?.abbr})`}
