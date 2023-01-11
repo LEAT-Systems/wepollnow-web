20f254667bec5b6efbedb957b8c1dee3401b5425
Stan <egbestanley84@yahoo.com>
2023-01-11 03:19:12 +0100

    Changed baseUrl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: "https://wepollnow.azurewebsites.net/",
+  baseURL: "http://api.wepollnow.com/",
 });
 
 export default axios;
--- src/store/baseUrl.js
+++ src/store/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "https://wepollnow.azurewebsites.net/";
+export const baseUrl = "http://api.wepollnow.com/";
